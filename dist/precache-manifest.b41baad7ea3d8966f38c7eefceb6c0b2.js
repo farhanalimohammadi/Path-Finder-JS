98d4c8188c0e1dddef857383f5290907992b8873
Farhan <farhanmohammadi045@gmail.com>
2026-02-20 12:24:18 +0330

    debug for deploy from farhan

---
+++ dist/precache-manifest.b41baad7ea3d8966f38c7eefceb6c0b2.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4ffe0ec6240f981766",
+    "url": "/css/app.2b502b48.css"
+  },
+  {
+    "revision": "d47983740cc9d1fa722fda8aea511a7e",
+    "url": "/img/building1.d4798374.png"
+  },
+  {
+    "revision": "ee52f081739634a05782e8ec9b42fdbc",
+    "url": "/img/building2.ee52f081.png"
+  },
+  {
+    "revision": "d6746a1097785098e1aa21578cc08cab",
+    "url": "/img/building3.d6746a10.png"
+  },
+  {
+    "revision": "141027f1dc69f7b26d421e2d5915cd42",
+    "url": "/img/camera.141027f1.svg"
+  },
+  {
+    "revision": "17eb5bbeaf4daa88f4025c134897117b",
+    "url": "/img/cross.17eb5bbe.svg"
+  },
+  {
+    "revision": "be570c434321c3a15e616d7e24908d01",
+    "url": "/img/ground-pebbles.be570c43.png"
+  },
+  {
+    "revision": "af02378ae987ce9944c923359b4e4737",
+    "url": "/img/ground.af02378a.png"
+  },
+  {
+    "revision": "f696b39f77d24b873c0aa81228dfe481",
+    "url": "/img/info.f696b39f.svg"
+  },
+  {
+    "revision": "5903b0b4486a749f76a5c74ff4360975",
+    "url": "/img/maze.5903b0b4.svg"
+  },
+  {
+    "revision": "c36d7275ba3ec0c6c9e364c8abf66300",
+    "url": "/img/path.c36d7275.svg"
+  },
+  {
+    "revision": "ab0ba8e44efbdf052f69b3bf428fb9de",
+    "url": "/img/perspective.ab0ba8e4.svg"
+  },
+  {
+    "revision": "956978fe4b5f2a405a1435f4af756315",
+    "url": "/img/reset-camera.956978fe.svg"
+  },
+  {
+    "revision": "6ec161a12f5d2edc3f8d0109daad3e20",
+    "url": "/img/setup.6ec161a1.svg"
+  },
+  {
+    "revision": "6948fed423821cb09846f89b014feb9b",
+    "url": "/img/street-view.6948fed4.svg"
+  },
+  {
+    "revision": "388984943d5d59a9d105d14c7e0ddd62",
+    "url": "/img/tutorial/deviceCamButton.jpg"
+  },
+  {
+    "revision": "61bd6989913c26435fcec48e529bd3cc",
+    "url": "/img/tutorial/deviceCamGrid.jpg"
+  },
+  {
+    "revision": "93ed0e71bcb5bca8c212c356ba77b120",
+    "url": "/img/tutorial/deviceCamPreview.jpg"
+  },
+  {
+    "revision": "6d02cd7c1029fc73e9c28fc81f89a713",
+    "url": "/img/tutorial/firstPerson.jpg"
+  },
+  {
+    "revision": "e49e5880b211b5da5424ab5dec21a775",
+    "url": "/img/tutorial/firstPerson2.jpg"
+  },
+  {
+    "revision": "3dd3f80a60e7b607f5e260c4bcb0abc8",
+    "url": "/img/tutorial/maze.jpg"
+  },
+  {
+    "revision": "14cc3f96a30f9cdf8822447b4dee932e",
+    "url": "/img/tutorial/maze2.jpg"
+  },
+  {
+    "revision": "7880f0b0616c8d317847dc74211ab8e4",
+    "url": "/img/tutorial/road.png"
+  },
+  {
+    "revision": "b73230f268cd8cf6e1fa43ba62f971f0",
+    "url": "/img/tutorial/selectAlgo.jpg"
+  },
+  {
+    "revision": "e4e5b83bdf81b4f153820fcfcc75a243",
+    "url": "/img/tutorial/setupWorld.jpg"
+  },
+  {
+    "revision": "bcceefb2182f80a43049cde604f9c572",
+    "url": "/img/tutorial/setupWorld2.jpg"
+  },
+  {
+    "revision": "5847f9ac04a518c81d7e3f57f671168c",
+    "url": "/img/tutorial/setupWorld2.png"
+  },
+  {
+    "revision": "8fe9284f6bfcc0dd2badcda8946e95aa",
+    "url": "/img/tutorial/speed.jpg"
+  },
+  {
+    "revision": "5f585a3ff026636710dbb43d03730320",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee4ffe0ec6240f981766",
+    "url": "/js/app.78ca690c.js"
+  },
+  {
+    "revision": "269b20849e8094179c81",
+    "url": "/js/chunk-vendors.d8c347c3.js"
+  },
+  {
+    "revision": "8c66aa81aac3177ee5e4ff60c69cd446",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
